c1c6be9d32516dea2e9e3c02122446ab42c0bfef
lucifer-886 <chethanb886@gmail.com>
2024-03-17 21:12:48 +0530

    done

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -4,13 +4,12 @@ import classes from './loading.module.css';
 
 export default function Loading() {
   const { isLoading } = useLoading();
-  if (!isLoading) return;
+  if (!isLoading) return null; // Return null if loading is complete
 
   return (
     <div className={classes.container}>
       <div className={classes.items}>
-        <img src="/loading.svg" alt="Loading!" />
-        <h1>Loading...</h1>
+        <img src="/loading4.svg" alt="Loading!" className={classes.svg} />
       </div>
     </div>
   );
